cf3e314b7636a13f59b6db3f6bacabef0b7e188b
johanavalos <johan.avalos.02@gmail.com>
2025-03-15 18:25:31 -0600

    feat: disable button if not valid inputs

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -19,7 +19,8 @@ function Signup() {
         <AuthLayout>
             <h1 className='form-title'>Create new account</h1>
             <Formik initialValues={initialValues} onSubmit={signup} validationSchema={signupSchema}>
-                <Form className="form-container">
+
+                {({ isValid }) => (<Form className="form-container">
                     <div className='field'>
                         <Field id="username" name="username" placeholder="Username" />
                         <ErrorMessage className='error' name='username' component="span" />
@@ -32,8 +33,8 @@ function Signup() {
                         <Field id="confirm-password" name="confirmPassword" placeholder="Confirm password" type="password" />
                         <ErrorMessage className='error' name='confirmPassword' component="span" />
                     </div>
-                    <button type='submit'>Sign up</button>
-                </Form>
+                    <button type='submit' disabled={!isValid}>Sign up</button>
+                </Form>)}
             </Formik>
             <a href='/login'>Already have an account? Sign in</a>
         </AuthLayout>
