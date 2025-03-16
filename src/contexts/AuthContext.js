c1e68f664ae177621b43441ba56fbf9544d4319a
johanavalos <johan.avalos.02@gmail.com>
2025-03-15 21:06:19 -0600

    feat: decode token and set auth context

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,5 @@
-import React, { useState, createContext, useContext } from "react";
+import React, { useState, createContext, useContext, useEffect } from "react";
+import { jwtDecode } from 'jwt-decode';
 
 const AuthContext = createContext()
 const AuthUpdateContext = createContext()
@@ -14,9 +15,33 @@ export function useAuthUpdate() {
 export function AuthContextProvider({ children }) {
     const [auth, setAuth] = useState({ "status": false, "username": null })
 
+    const [decoding, setDecoding] = useState(true)
+
+    useEffect(() => {
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            try {
+                const decoded = jwtDecode(token);
+                const currentTime = Date.now() / 1000; // Current time in seconds
+                if (decoded.exp > currentTime) {
+                    setAuth({"status": true, "username": decoded.sub})
+                } else {
+                    console.log("TOKEN EXPIRED")
+                }
+            } catch (error) {
+                console.error('Invalid token:', error);
+            }
+        } else {
+            console.log('No token found in localStorage');
+        }
+        setDecoding(false)
+
+    }, [])
+
+
     return <AuthContext.Provider value={auth}>
         <AuthUpdateContext value={setAuth}>
-            {children}
+            {!decoding? children: <></>}
         </AuthUpdateContext>
     </AuthContext.Provider>
 }
