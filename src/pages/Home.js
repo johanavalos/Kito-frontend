e5cc4dc2795e23c3d3985613fd73b52d30809a78
johanavalos <johan.avalos.02@gmail.com>
2025-03-17 19:03:00 -0600

    feat: add profile pic and logout button to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,39 @@
-import React from 'react'
-import { useAuth } from '../contexts/AuthContext'
+import React, { useEffect, useState } from 'react'
+import { useAuth, useAuthUpdate } from '../contexts/AuthContext'
+import { useNavigate, Link } from 'react-router-dom'
+
+import ProfilePicture from '../components/ProfilePicture'
 
 function Home() {
     const authContext = useAuth()
+    const authContextUpdate = useAuthUpdate()
+    const navigate = useNavigate()
+    const [checking, setChecking] = useState(true)
+
+    useEffect(() => {
+        if (!authContext.status) {
+            navigate("/login")
+        }
+        setChecking(false)
+    }, [authContext, navigate])
+
+    const logout = () => {
+        localStorage.removeItem("accessToken");
+        navigate("/login");
+        authContextUpdate({ "status": false, "username": null })
+    }
+
     return (
         <>
-            <h1>Hi, {authContext.username}</h1>
-            <a href='/login'>Sign in</a>
-            <a href='/signup'>Sign up</a>
+            {(checking) ? <></> : <>
+                <title>Home</title>
+                <h1>Hi, {authContext.username}</h1>
+                <ProfilePicture/>
+                <Link to='/login'>Sign in</Link>
+                <Link to='/signup'>Sign up</Link>
+                <button onClick={logout}>Logout</button>
+            </>
+            }
         </>
     )
 }
